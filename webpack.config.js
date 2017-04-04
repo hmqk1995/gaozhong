bab7ceb64dd0ee9cd0d805aa3d8d2c3511e470e9
钱锟 <qiankun@wepiao.com>
2017-04-04 19:45:25 +0800

    1st commit with webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+  entry: './src/script/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist/js'),
+    filename: 'bundle.js'
+  }
+};
