70fdf788d65c7f4ce3f6461280f1130583c8f937
钱锟 <qiankun@wepiao.com>
2017-04-27 21:21:44 +0800

    nav left

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,10 +10,28 @@
     });
   }
 
+  function changeText (selector) {
+    var text = $(selector).text();
+    console.log(text);
+    $(selector).hover(
+      function(){
+        if (text == '参 观') {$(this).text('VISIT')};
+        if (text == '影 像') {$(this).text('IMAGES')};
+        if (text == '争 鸣') {$(this).text('VIEW POINTS')};
+        if (text == '经 历') {$(this).text('EXPERIENCE')};
+    },function(){
+      $(this).text(text);
+    });
+  }
+
   hover('.quater-mask-1');
   hover('.quater-mask-2');
   hover('.quater-mask-3');
   hover('.quater-mask-4');
 
+  changeText('#hoverlink1');
+  changeText('#hoverlink2');
+  changeText('#hoverlink3');
+  changeText('#hoverlink4');
 
 }());
