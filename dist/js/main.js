138fadfedf334ecf52e63d69c7cee5fbaa55c3b0
钱锟 <qiankun@wepiao.com>
2017-05-04 21:03:09 +0800

    index animate right

--- dist/js/main.js
+++ dist/js/main.js
@@ -84,9 +84,49 @@
   });
 
   $('.index-right').hover(function(){
+
+    // 让青岛二中校徽压下
+    $('.index-left').css('z-index', '99999');
+
     $('#ezheading').show();
+    TweenMax.to('#logo-half-e', 0.8, {
+      opacity: 1,
+      delay: 0.2,
+      left: 0
+    });
+    TweenMax.to('#head-text-right', 0.8, {
+      transform: 'rotate(0deg)',
+      delay: 0.2,
+      opacity: 1
+    });
+    TweenMax.to('#head-hand-right', 1, {
+      opacity: 1,
+      delay: 0.8
+    });
+
+    TweenMax.to('#head-hand-right', 0.5, {
+      repeat: -1,
+      yoyo: true,
+      scale: 1.1
+    });
+
   }, function(){
+
+    $('.index-left').css('z-index', 'initial');
+
     $('#ezheading').hide();
+    TweenMax.to('#logo-half-e', 0, {
+      opacity: 0,
+      left: '-100px'
+    });
+    TweenMax.to('#head-text-right', 0, {
+      transform: 'rotate(-24deg)',
+      opacity: 0
+    });
+    TweenMax.to('#head-hand-right', 0, {
+      opacity: 0,
+      scale: 1
+    });
   });
 
 }());
